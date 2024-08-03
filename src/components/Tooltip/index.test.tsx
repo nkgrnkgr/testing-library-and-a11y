3c8399da9cf5ab5f7e302b949f8a255ba356cdf6
nkgrnkgr <beefupme@gmail.com>
2024-08-03 21:42:00 +0900

    fix

--- src/components/Tooltip/index.test.tsx
+++ src/components/Tooltip/index.test.tsx
@@ -1,4 +1,4 @@
-import { logDOM, screen, within } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { describe, expect, it } from "vitest";
 import { TooltipDisplay } from ".";
