a145f267530579e108def6560e101d18b9080d2d
nkgrnkgr <beefupme@gmail.com>
2024-08-03 12:43:56 +0900

    add test with userEvent

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import { describe, expect, it } from "vitest";
 import { App } from "./App";
 
@@ -7,5 +8,18 @@ describe("App", () => {
     render(<App />);
     const title = screen.getByRole("heading");
     expect(title.textContent).toBe("This is App");
+  });
+
+  it("should increment count", async () => {
+    const user = userEvent.setup();
+    render(<App />);
+
+    const button = screen.getByRole("button", {
+      name: "Click",
+    });
+    const count = screen.getByLabelText("Count:");
+    expect(count.textContent).toBe("0");
+    await user.click(button);
+    expect(count.textContent).toBe("1");
   });
 });
