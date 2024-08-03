e6ef7700c2114eb7dd19fd517b5f59aff33152ad
nkgrnkgr <beefupme@gmail.com>
2024-08-03 14:17:05 +0900

    update

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,10 @@
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vitest/config';
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts']
-  }
+    environment: "jsdom",
+    setupFiles: ["./vitest-setup.ts"],
+  },
 });
